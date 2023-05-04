c810f48d171505d8304b7bff7251896ad09092e5
Olli Paloviita <olli.paloviita@gmail.com>
2023-05-04 16:30:43 +0300

    Fix mouse move relative again

--- src/linux/simulate.rs
+++ src/linux/simulate.rs
@@ -4,25 +4,42 @@ use crate::rdev::{Button, EventType, SimulateError};
 use std::convert::TryInto;
 use std::os::raw::{c_char, c_int, c_void};
 use std::ptr::null;
+use std::sync::Mutex;
 use x11::xlib;
 use x11::xtest;
 
 type Window = c_int;
+
 type Xdo = *const c_void;
+#[derive(Clone)]
+struct WrapperType(Xdo);
+
+unsafe impl Send for WrapperType {}
+unsafe impl Sync for WrapperType {}
+impl Copy for WrapperType {}
+
 const CURRENT_WINDOW: c_int = 0;
 use std::ptr;
+use lazy_static::lazy_static;
 
 #[link(name = "xdo")]
 extern "C" {
-    fn xdo_new(display: *const c_char) -> Xdo;
-    fn xdo_move_mouse_relative(xdo: Xdo, x: c_int, y: c_int) -> c_int;
+    fn xdo_new(display: *const c_char) -> WrapperType;
+    fn xdo_move_mouse_relative(xdo: WrapperType, x: c_int, y: c_int) -> c_int;
     fn xdo_get_mouse_location2(
-        xdo: Xdo,
+        xdo: WrapperType,
         x: *mut c_int,
         y: *mut c_int,
         screen: *mut c_int,
         window: *mut Window,
     ) -> c_int;
+}
+
+
+lazy_static! {
+    static ref XDO: WrapperType = {
+        unsafe { xdo_new(ptr::null()) }
+    };
 }
 
 fn mouse_location() -> (i32, i32) {
@@ -32,7 +49,7 @@ fn mouse_location() -> (i32, i32) {
     let mut unused_window_index = CURRENT_WINDOW;
     unsafe {
         xdo_get_mouse_location2(
-            xdo_new(ptr::null()), // TODO save one
+            *XDO, // TODO save one
             &mut x,
             &mut y,
             &mut unused_screen_index,
@@ -88,14 +105,15 @@ unsafe fn send_native(event_type: &EventType, display: *mut xlib::Display) -> Op
             //     xlib::XWarpPointer(display, 0, root, 0, 0, 0, 0, *x as i32, *y as i32);
         }
         EventType::MouseMoveRelative { x, y } => {
-            //TODO: replace with clamp if it is stabalized
             unsafe {
                 xdo_move_mouse_relative(
-                    xdo_new(ptr::null()), // TODO save one
+                    *XDO,
                     (*x as i32) as c_int,
                     (*y as i32) as c_int,
-                )
+                );
             }
+            let result: c_int = 1;
+            result
         }
         EventType::Wheel { delta_x, delta_y } => {
             let code_x = if *delta_x > 0 { 7 } else { 6 };
